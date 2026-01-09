a6bdbea13ab298ca39ec2c4a686c6472e66d7ec6
Navjinder Singh <206590909+navjindersingh01@users.noreply.github.com>
2026-01-10 04:53:28 +0530

    Final commit

--- logic.py
+++ logic.py
@@ -68,15 +68,102 @@ class NumberToRoman():
         
         with open("roman.json",'r') as f:
             self.romans = json.load(f)
+        
+        with open("unit_numbers.json") as f:
+            self.unit_romans = json.load(f)
+        
+    def add_vinculum(self,string):
+        return "".join([char + '\u0305' for char in string])
+    
+    def to_roman_helper(self,number):
+        roman_number = str()
+        roman_list = []
+
+
+        if 1 <= (number // 100) < 3:
+            count = number // 100
+            for _ in range(count):
+                roman_list.append("C")
+        
+        elif 1 <= (number // 10) < 4:
+            count = number // 10
+            for _ in range(count):
+                roman_list.append("X")
+        
+
+        roman_number = ''.join(roman_list)
+
+        return roman_number
     
     def to_roman(self):
-        if len(self.a) == 1:
-            if int(self.a) in self.romans.values():
-                return 
+        length = len(self.a)
+        num_list = list(self.a)
+        places_list = []
+        roman_list = []
+
+        if length == 1:
+            return self.unit_romans[self.a]
+        
+        for i in num_list:
+            places_list.append(int(i) * (10**(length-1)))
+            length -= 1
+        
+        for i in places_list:
+            if len(str(i)) >=3 and i//1000:
+                if 1 < i // 1000 < 4: 
+                    for i in range(i//1000):
+                        roman_list.append("M")
+                else:
+                    a = i//1000
+                    roman_list.append(self.add_vinculum(self.unit_romans[str(a)]))
+            elif len(str(i)) == 3 and i//500:
+                if i // 500:
+                    remainder = i - 500
+                    if remainder // 100 < 4:
+                        roman_numeral_remainder = self.to_roman_helper(remainder)
+                        roman_list.append("D"+roman_numeral_remainder)
+                    else: 
+                        roman_list.append("CM")
+
+            elif len(str(i)) == 3 and i//100:
+                if i // 100:
+                    remainder = i - 100
+                    if remainder // 100 < 3:
+                        roman_numeral_remainder = self.to_roman_helper(remainder)
+                        roman_list.append("C" + roman_numeral_remainder)
+                    else: 
+                        roman_list.append("CD")
+                        
+            elif len(str(i)) == 2: 
+                if i // 50 :
+                    remainder = i - 50
+                    if remainder // 10  < 4:
+                        roman_numeral_remainder = self.to_roman_helper(remainder)
+                        roman_list.append("L" + roman_numeral_remainder)
+                    else:
+                        roman_list.append("XC")
+                else:
+                    remainder = i - 10
+                    if remainder // 10  < 3:
+                        roman_numeral_remainder = self.to_roman_helper(remainder)
+                        roman_list.append("X" + roman_numeral_remainder)
+                    else:
+                        roman_list.append("XL")
+            
+            elif len(str(i)) ==1:
+                roman_list.append(self.unit_romans[str(i)])
+        
+        roman_number = ''.join(roman_list)
+        
+        return roman_number
+
+
+            
+            
 
 if __name__ == "__main__":
-    roman = 'MDCCCLXXXVIIV'
-    num1 = RomanToNumber(roman)
-    print(num1.to_num())
-    print(num1)
+    roman = 3888
+    num1 = NumberToRoman(roman) # CCCLXXXVIII
+    print(num1.to_roman())
+
     
--- main.py
+++ main.py
@@ -4,14 +4,21 @@ a = l.RomanToNumber(input("Enter 1st Roman Number: ")).to_num()
 b = l.RomanToNumber(input("Enter 2nd Roman Number: ")).to_num()
 
 operator = input("\nEnter the Operator: ")
-
 solution = l.Calculator(a,b)
 
+
+
+
+
 if operator == "+":
-    print(solution.add())
+    result = solution.add()
 elif operator == '-':
-    print(solution.sub())
+    result = solution.sub()
 elif operator == '*':
-    print(solution.mul())
+    result = solution.mul()
 else:
     print("Use only *,-,+")
+
+converter = l.NumberToRoman(result)
+
+print(f"Result: {converter.to_roman()}")
